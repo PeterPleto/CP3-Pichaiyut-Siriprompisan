ef17544abbae6111cd5dd40458b9a2c23d4e16f4
PeterPleto <39252265+PeterPleto@users.noreply.github.com>
2019-08-22 21:41:50 +0700

    Add Code

---
+++ Exercise4_Pichaiyut_S.py
@@ -0,0 +1,9 @@
+fe_score=4.0
+gb_score=4.0
+int_to_com_sys_score=4.0
+comprog_score=4.0
+print("--- Your Score ---")
+print("Foundation English :",fe_score)
+print("General Business :",fe_score)
+print("Introduction to Computer Systems :",fe_score)
+print("Computer Programming :",fe_score)
