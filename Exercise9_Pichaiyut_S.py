57c4eb9d1502a82f190085ebbfd6e1c214c19c2e
PeterPleto <39252265+PeterPleto@users.noreply.github.com>
2019-08-23 21:51:36 +0700

    EXERCISE 9 ADD

---
+++ Exercise9_Pichaiyut_S.py
@@ -0,0 +1,6 @@
+username = input("Username: ")
+password = input("Password: ")
+while username != "admin"and password != "pppp":
+    username = input("Username: ")
+    password = input("Password: ")
+    print("Done")
